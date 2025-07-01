555f70deff487bc03b9aca92f8a170d69bb88bf6
Gideon <gidyhormes98@gmail.com>
2025-07-01 05:05:56 +0100

    nxbd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { Navbar, Footer } from "@/components";
 
 // sections
 import Hero from "./hero";
-import Clients from "./clients";
+//import Clients from "./clients";
 import Skills from "./skills";
 import Projects from "./projects";
 import Resume from "./resume";
-import Testimonial from "./testimonial";
-import PopularClients from "./popular-clients";
+//import Testimonial from "./testimonial";
+//import PopularClients from "./popular-clients";
 import ContactForm from "./contact-form";
 
 export default function Portfolio() {
@@ -16,14 +16,16 @@ export default function Portfolio() {
     <>
       <Navbar />
       <Hero />
-      <Clients />
       <Skills />
       <Projects />
       <Resume />
-      <Testimonial />
-      <PopularClients />
       <ContactForm />
       <Footer />
     </>
   );
 }
+
+
+//      <Clients />
+//      <Testimonial />
+//      <PopularClients />
